1917fc5af57b8eb833619e8496f0b0578a17c1aa
Gabriel De Melo <30329012+Biellmelo@users.noreply.github.com>
2022-02-03 00:16:21 -0300

    Finalizando aula 1

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-sss
+<?php
+require_once('../vendor/autoload.php');
+require_once('../app/functions/functions.php');
+
+use app\controller\TesteController;
+(new \app\core\RouterCore()); 
+$controller = new TesteController();
+
